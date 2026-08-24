b9263de14c592cc0549d3a8b73fd93a457fcb8cf
eliezer <eliezermasiala2000@gmail.com>
2026-08-24 18:31:27 +0100

    style(working): format title across two lines

--- components/sections/Working.tsx
+++ components/sections/Working.tsx
@@ -10,8 +10,8 @@ export function Working() {
     return (
         <Section className="bg-cmyk pt-0">
             <Container className="flex flex-col justify-center items-center h-[40vh]">
-                <div className="w-full md:w-[70%] scale-105 pt-8 md:pt-5">
-                    <h3 className="text-4xl md:text-5xl leading-tight text-center text-primary font-sans font-bold">
+                <div className="w-full md:w-[70%] scale-105 py-8 md:pt-5">
+                    <h3 className="text-4xl whitespace-pre-line md:text-5xl leading-tight text-center text-primary font-sans font-bold">
                         {t("Working-title")}
                     </h3>
                     <p className="text-text/80 mt-5 text-center text-[18px] md:text-xl font-sans">
